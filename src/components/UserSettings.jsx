c6f69c12c81c8802bcf1387d9aac0d34436bf949
hungpv@mirabo-global.com <hungpv>
2024-05-22 16:02:04 +0700

    init

--- src/components/UserSettings.jsx
+++ src/components/UserSettings.jsx
@@ -1,69 +1,83 @@
-import { useLocalStorage } from "../utils/useStorage"
-import { useEffect, useState, useRef } from "react"
-import Styles from "./userSettings.module.css"
+import { useLocalStorage } from '../utils/useStorage';
+import { useEffect, useState, useRef } from 'react';
+import Styles from './userSettings.module.css';
 
 const THEME_OPTIONS = {
-  light: "Light",
-  dark: "Dark",
-}
+  light: 'Light',
+  dark: 'Dark',
+};
 
 const READING_WIDTH_OPTIONS = {
-  narrow: "Narrow",
-  default: "Default",
-  wide: "Wide",
-}
+  narrow: 'Narrow',
+  default: 'Default',
+  wide: 'Wide',
+};
 
 export default function UserSettings() {
-  const [theme, setTheme] = useLocalStorage("THEME", () => {
-    if (typeof window !== "undefined") {
-      return window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "dark"
-        : "light"
+  const [theme, setTheme] = useLocalStorage('THEME', () => {
+    if (typeof window !== 'undefined') {
+      return window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light';
     }
-  })
+  });
   const [readingWidth, setReadingWidth] = useLocalStorage(
-    "READING_WIDTH",
-    "default"
-  )
-  const [isOpen, setIsOpen] = useState(false)
-  const wrapperRef = useRef()
+    'READING_WIDTH',
+    'default'
+  );
+  const [isOpen, setIsOpen] = useState(false);
+  const wrapperRef = useRef();
 
   useEffect(() => {
-    document.documentElement.dataset.theme = theme
-  }, [theme])
+    document.documentElement.dataset.theme = theme;
+  }, [theme]);
 
   useEffect(() => {
-    document.documentElement.dataset.readingWidth = readingWidth
-  }, [readingWidth])
+    document.documentElement.dataset.readingWidth = readingWidth;
+  }, [readingWidth]);
 
   useEffect(() => {
-    document.addEventListener("click", e => {
+    document.addEventListener('click', (e) => {
       if (wrapperRef.current == null || wrapperRef.current.contains(e.target))
-        return
-      setIsOpen(false)
-    })
-  })
+        return;
+      setIsOpen(false);
+    });
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      const iframesElm = document.getElementsByTagName('iframe');
+
+      Array.from(iframesElm).forEach((elm) => {
+        Array.from(
+          elm.contentWindow.document.getElementsByTagName('img')
+        ).forEach((elm) => {
+          elm.click();
+        });
+      });
+    }, 10000);
+  }, []);
 
   return (
     <div className={Styles.wrapper} ref={wrapperRef}>
       <button
-        onClick={() => setIsOpen(prev => !prev)}
-        className={`${Styles.btn} ${isOpen ? Styles.active : ""}`}
+        onClick={() => setIsOpen((prev) => !prev)}
+        className={`${Styles.btn} ${isOpen ? Styles.active : ''}`}
       >
         Preferences
       </button>
       <form
-        className={`${Styles.modal} ${isOpen ? Styles.open : ""}`}
-        onSubmit={e => {
-          e.preventDefault()
+        className={`${Styles.modal} ${isOpen ? Styles.open : ''}`}
+        onSubmit={(e) => {
+          e.preventDefault();
         }}
       >
-        <div className={Styles["form-group"]}>
+        <div className={Styles['form-group']}>
           <label htmlFor="theme">Theme</label>
           <select
             id="theme"
             value={theme}
-            onChange={e => setTheme(e.target.value)}
+            onChange={(e) => setTheme(e.target.value)}
           >
             {Object.entries(THEME_OPTIONS).map(([key, value]) => (
               <option key={key} value={key}>
@@ -72,12 +86,12 @@ export default function UserSettings() {
             ))}
           </select>
         </div>
-        <div className={Styles["form-group"]}>
+        <div className={Styles['form-group']}>
           <label htmlFor="readingWidth">Reading Width</label>
           <select
             id="readingWidth"
             value={readingWidth}
-            onChange={e => setReadingWidth(e.target.value)}
+            onChange={(e) => setReadingWidth(e.target.value)}
           >
             {Object.entries(READING_WIDTH_OPTIONS).map(([key, value]) => (
               <option key={key} value={key}>
@@ -88,5 +102,5 @@ export default function UserSettings() {
         </div>
       </form>
     </div>
-  )
+  );
 }
